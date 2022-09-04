904cf2615f93920c3efb01a72a7ba8ec23ff5b20
Aleksandr <golyshkoa_gaa@mail.ru>
2022-09-04 20:08:34 +0300

    Add solution Syntax from task 0208

--- 1.JavaSyntax/src/com/javarush/task/pro/task02/task0214/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0214/Solution.java
@@ -9,7 +9,27 @@ import java.util.Scanner;
 public class Solution {
 
     public static void main(String[] args) {
-        //напишите тут ваш код
+
+        Scanner a = new Scanner(System.in);
+        String A = a.nextLine();
+        String B = a.nextLine();
+        String C = a.nextLine();
+
+        System.out.println(C);
+        System.out.println(B.toUpperCase());
+        System.out.println(A.toLowerCase());
 
     }
 }
+// String A = a.nextLine();
+//        System.out.println("Строка Номер Раз".toLowerCase());
+//
+//        String B = a.nextLine();
+//        System.out.println("Вторая строка".toUpperCase());
+//
+//        String C = a.nextLine();
+//        System.out.println("Строка 3");
+//
+//        System.out.println(C);
+//        System.out.println(B);
+//        System.out.println(A);
