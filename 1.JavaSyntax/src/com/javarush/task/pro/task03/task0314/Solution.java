f1af9ddaaf20822d23fecd910998726a597538a5
Aleksandr <golyshkoa_gaa@mail.ru>
2022-09-08 02:15:37 +0300

    add solution Syntax from task 0313

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task03/task0314/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.task.pro.task03.task0314;
+
+import java.util.Scanner;
+
+/* 
+Сломанная клавиатура
+*/
+
+public class Solution {
+    public static String secret = "AmIGo";
+
+    public static void main(String[] args) {
+        Scanner name = new Scanner(System.in);
+        String a = name.nextLine();
+         if (secret.equalsIgnoreCase(a)) {
+             System.out.println("доступ разрешен");
+         } else {
+        System.out.println("доступ запрещен");
+         }
+    }
+}
